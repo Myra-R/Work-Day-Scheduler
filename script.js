dbb6496ac8af994cf4c0e1d9e9850e24a82300a6
Myra Rozmiarek <11marozmi@gmail.com>
2020-10-01 18:56:23 -0500

    fixed bug to get planner showing in full

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var curDay = document.querySelector("#currentDay");
 
- document.querySelector("#currentDay").textContent=moment().format('LLL');
- var currentTime=moment().format('HH')
- console.log(currentTime)  
- var hour = [ 9, 10, 11, 12, 13, 14, 15, 16, 17];
+document.querySelector("#currentDay").textContent = moment().format('LLL');
+var currentTime = moment().format('HH')
+console.log(currentTime)
+var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 /*
  <div class="row time-block">
         <div class="col-md-1">
@@ -16,85 +16,85 @@ var curDay = document.querySelector("#currentDay");
 </div>
 */
 
+//remove 18-20 after testing 
+
+function createPlan() {
+    //input area works, make forloop work
+    for (var i = 0; i < hour.length; i++) {
+        console.log(hour.length);
+
+        var d1 = document.createElement("div");
+        //<div></div>
+        d1.setAttribute("class", "row time-block")
+        //<div class="row time-block"></div>
+
+        var d2 = document.createElement("div");
+        d2.setAttribute("class", "col-md-1");
+
+        var p = document.createElement("p");
+        p.textContent = hour[i];
+        if (hour[i] < 12) {
+            p.textContent = hour[i] + " AM";
+        }
+        if (hour[i] == 12) {
+            p.textContent = hour[i] + "PM";
+        }
+        if (hour[i] > 12) {
+            p.textContent = hour[i] - 12 + "PM";
+
+        }
+
+        var textarea = document.createElement("textarea");
+        //textarea.setAttribute("class","description col-md-10");
+        //if current time 
+        if (currentTime == hour[i]) {
+            textarea.setAttribute("class", "description col-md-10 present " + hour[i]);
+
+        }
+        if (currentTime > hour[i]) {
+            textarea.setAttribute("class", "description col-md-10 past " + hour[i]);
+
+        }
+        if (currentTime < hour[i]) {
+            textarea.setAttribute("class", "description col-md-10 future " + hour[i]);
+
+        }
+        //currentTime==hour
+        //add class present
+        //if(past) currentTime-hour>0
+
+        var btn = document.createElement("button");
+        btn.setAttribute("id", hour[i])
+        btn.setAttribute("class", "saveBtn col-md-1 btn")
+
+        var iframe = document.createElement("i");
+        iframe.setAttribute("class", "fas fa-save");
+        document.querySelector(".container").appendChild(d1)
+        d2.appendChild(p);
+        btn.appendChild(iframe);
+
+        d1.appendChild(d2);
+        d1.appendChild(textarea);
+        d1.appendChild(btn);
+
+   
+        console.log("work")
+    }
 
 
-function createPlan(){
-//ineneput area works, make forloop work
-for(var i=0;i<hour.length;i++){
-    console.log(hour.length);
+    //create an onclick event.
+    //grab the userinput and hour
+    //set to local storage
+    //.saveBtn
+    document.querySelector(".saveBtn").onclick = function () {
+        console.log(this.getAttribute("id"));
+        console.log(this.parentElement.children[1].value);
+        localStorage.setItem(this.getAttribute("id"), this.parentElement.children[1].value);
 
-var d1=document.createElement("div");
-//<div></div>
-d1.setAttribute("class","row time-block")
-//<div class="row time-block"></div>
+    };
 
-var d2=document.createElement("div");
-d2.setAttribute("class","col-md-1");
-
-var p=document.createElement("p");
-p.textContent=hour[i];
-if(hour[i]<12){
-    p.textContent=hour[i]+" AM";
-}
-if(hour[i]==12){
-    p.textContent=hour[i]+"PM";
-}
-if(hour[i]>12){
-    p.textContent=hour[i]-12+"PM";
-
-}
-
-var textarea=document.createElement("textarea");
-//textarea.setAttribute("class","description col-md-10");
-//if current time 
-if(currentTime==hour[i]){
-    textarea.setAttribute("class","description col-md-10 present "+hour[i]);
-
-}
-if(currentTime>hour[i]){
-    textarea.setAttribute("class","description col-md-10 past "+hour[i]);
-
-}
-if(currentTime<hour[i]){
-    textarea.setAttribute("class","description col-md-10 future "+hour[i]);
-
-}
-    //currentTime==hour
-    //add class present
-//if(past) currentTime-hour>0
-
-var btn=document.createElement("button");
-btn.setAttribute("id",hour[i])
-btn.setAttribute("class","saveBtn col-md-1 btn")
-
-var i=document.createElement("i");
-i.setAttribute("class","fas fa-save");
-
-d2.appendChild(p);
-btn.appendChild(i);
-
-d1.appendChild(d2);
-d1.appendChild(textarea);
-d1.appendChild(btn);
-
-document.querySelector(".container").appendChild(d1)
-console.log("work")
-}
-
-
-//create an onclick event.
-//grab the userinput and hour
-//set to local storage
-//.saveBtn
-document.querySelector(".saveBtn").onclick = function(){
-   console.log(this.getAttribute("id"));
-   console.log(this.parentElement.children[1].value);
-   localStorage.setItem(this.getAttribute("id"), this.parentElement.children[1].value);
-
-};
-
-//document.querySelector(".9").value(localStorage.getItem("9"));
- console.log(localStorage.getItem("9"))
+    document.querySelector(".9").value(localStorage.getItem("9"));
+    console.log(localStorage.getItem("9"))
 }
 
 createPlan();
@@ -106,7 +106,7 @@ createPlan();
 // }
 
 // $(document).ready(function(){
-    
+
 //     for(var i = 0; i < hours.length; i++){
 //         var hourBlock = $("<div>");
 //         hourBlock.addClass("time-table");
